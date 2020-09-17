8fd2eafa803ed9a7ef201f6da29644439493d1bd
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-09-16 21:42:08 -0400

    knex

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/users.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  }
+
+};
