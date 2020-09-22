95651b01cfb864564681a730bebd1e65e398441e
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-09-22 19:48:07 -0400

    updated owners router and model

--- owners/owners-router.js
+++ owners/owners-router.js
@@ -3,17 +3,17 @@ const Owners = require("./owners-model");
 
 const router = express.Router();
 
+//get owner
 router.get("/", (req, res, next) => {
     try {
-        res.json({
-            data: req.user
-        });
+        res.json(req.user);
     } catch (err){
         next(err);
     }
 });
 
-router.get("/items", (req, res, next) => {
+//get listings
+router.get("/items", async (req, res, next) => {
     try {
         const items = await Owners.getItems(req.user.id);
         
@@ -22,7 +22,58 @@ router.get("/items", (req, res, next) => {
                 message: "No current listings"
             })
         }
-        //res.json({ data: items }) ?????
+        res.json(items)
+
+    } catch (err) {
+        next(err);
+    }
+})
+
+//get single listing
+router.get("/items/:id", async (req, res, next) => {
+    try {
+        res.status(200).json(req.item);
+    } catch (err) {
+        next(err);
+    }
+})
+
+//add listing
+router.post("/items", async (req, res, next) => {
+    try {
+        const { name, price, description } = req.body;
+        const newListing = await Owners.add({
+            name,
+            price,
+            description,
+            owner_id: req.user.id
+        });
+
+        res.status(201).json(newListing);
+    } catch (err) {
+        next(err);
+    }
+})
+
+//update listing
+router.put("/items/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+        const updatedListing = await Owners.update(id, updates);
+
+        res.json(updatedListing);
+    } catch (err) {
+        next(err);
+    }
+})
+
+//delete listing
+router.delete("/items/:id", async (req, res, next) => {
+    try {
+        const count = await Owners.remove(req.params.id);
+
+        res.json({message: `item successfully deleted. ${count}`})
     } catch (err) {
         next(err);
     }
