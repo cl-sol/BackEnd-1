a97233d9c78615870726fc4ede11c4a10ae5c688
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-09-21 21:35:27 -0400

    merged owners and renters bc reasons

---
+++ owners/owners-model.js
@@ -0,0 +1,29 @@
+const db = require("../data/dbconfig");
+
+module.exports = {
+    findById,
+    getItems,
+    add,
+    update,
+    remove
+}
+
+function findById(id) {
+    return db("")
+};
+
+function getItems(owner_id) {
+    asdf
+};
+
+function add(item) {
+    adf
+};
+
+function update(id, update) {
+    dsafd
+};
+
+function remove(id) {
+    sdaf
+};
